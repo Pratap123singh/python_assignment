fed18e134cc5fdf45feb93235ed810299ccda3c4
pratap123singh <pranjalpratapsingh007@gmail.com>
2024-05-02 01:17:26 +0530

    list

---
+++ list/greater_than.py
@@ -0,0 +1,27 @@
+l1 = [ 0 , 1]
+x= l1[0]
+print("x: ", x)
+print("l1: ", l1)
+
+x, l1[x] = 1, 300
+
+print("x: ", x)
+print("l1: ", l1)
+
+x, l1[x] = 0, 200
+
+print("x: ", x)
+print("l1: ", l1)
+
+x, l1[x] = -1, 100
+
+print("x: ", x)
+print("l1: ", l1)
+
+
+
+
+comp = [1, 2, ('abc', 'ab')]   < [1, 2, ('abc', 'a'), 4]
+print(comp)
+comp1 = [1, 2, ('abc', 'a'),4]   < [1, 2, ('abc', 'a')]
+print(comp1)
